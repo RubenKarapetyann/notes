d90f6b8b2c6d6abe357c4741d836e7aef268efd2
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-09-28 20:46:37 +0400

    create a route to create a new note in the database

--- app/api/create/route.ts
+++ app/api/create/route.ts
@@ -1,7 +1,18 @@
 import { NextResponse } from "next/server";
+import pool from "@/utils/api-utils/pool";
 
 export async function POST(req:Request){
-    const { title, body } = await req.json()
-    console.log(title,body);
-    return NextResponse.json({title,body})
+    try{
+        const { title, body } = await req.json()
+        const date = new Date().getTime()
+
+        await pool.query(
+            "INSERT INTO notes(title, body, publicDate, likes, dislikes) VALUES (?,?,?,?,?)",
+            [title, body, date, 0, 0]
+        )
+
+        return NextResponse.json({message : "ok", access : true, redirect : "/"})
+    }catch(err){
+        console.log(err)
+    }
 }
